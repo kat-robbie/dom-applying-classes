9931ec84a048bb398279ed21b177d82e4a5b75f4
Kat Robbie <katherine.robbie@gmail.com>
2017-01-19 17:05:04 +1300

    blue green circle done

--- classes.js
+++ classes.js
@@ -1,13 +1,14 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+   one()
+   two()
+
+  //Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
@@ -18,7 +19,12 @@ function one () {
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
 
